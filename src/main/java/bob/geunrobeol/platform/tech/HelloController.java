c67d8993730efd7a31471581330bbf69020c548f
hongroklim <hongrr123@gmail.com>
2023-11-06 01:13:56 +0900

    Init Repo

---
+++ src/main/java/bob/geunrobeol/platform/tech/HelloController.java
@@ -0,0 +1,13 @@
+package bob.geunrobeol.platform.tech;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello() {
+        return "{\"message\": \"Hello, World!\"}";
+    }
+}
---
+++ src/main/java/bob/geunrobeol/platform/tech/PlatformTechApplication.java
@@ -0,0 +1,13 @@
+package bob.geunrobeol.platform.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformTechApplication.class, args);
+	}
+
+}
