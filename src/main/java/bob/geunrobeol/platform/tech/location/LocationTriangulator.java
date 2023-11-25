9ce26b05758220c590c72fd390323d77cfb40cfe
suhyun-yun <yshsu0223@gmail.com>
2023-11-25 14:24:34 +0900

    Fix : LocationTriangulator & test code

--- src/main/java/bob/geunrobeol/platform/tech/location/LocationTriangulator.java
+++ src/main/java/bob/geunrobeol/platform/tech/location/LocationTriangulator.java
@@ -1,20 +1,16 @@
 package bob.geunrobeol.platform.tech.location;
 
 import org.springframework.stereotype.Service;
-
 import java.util.AbstractMap;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import bob.geunrobeol.platform.tech.vo.BeaconRecord;
 import bob.geunrobeol.platform.tech.vo.PositionRecord;
 import bob.geunrobeol.platform.tech.vo.ScannerData;
-
 import java.awt.geom.Point2D;
-
+import java.util.HashMap;
 
 
 /**
@@ -23,6 +19,25 @@ import java.awt.geom.Point2D;
 @Service
 public class LocationTriangulator implements ILocationEstimator {
 
+    private Map<String, Point2D.Double> scannerPositions = new HashMap<>();
+
+    public LocationTriangulator() {
+        initializeScannerPositions();
+    }
+
+    /**
+     * 초기 스캐너 위치를 설정한다. 
+     * 이 메소드는 각 스캐너의 고유한 식별자와 해당 스캐너의 좌표를 매핑하여 저장한다.
+     * 이 매핑은 삼각측량을 수행할 때 사용된다.
+     */
+    private void initializeScannerPositions() {
+        scannerPositions.put("A", new Point2D.Double(0, 0));
+        scannerPositions.put("B", new Point2D.Double(400, 0));
+        scannerPositions.put("C", new Point2D.Double(0, 400));
+        scannerPositions.put("D", new Point2D.Double(400, 400));
+    }
+
+    
     /**
      * Beacon별 데이터(N개)로부터 Beacon의 위치(1개)를 추정한다.
      * @param beaconRecord Beacon별 데이터
@@ -30,7 +45,6 @@ public class LocationTriangulator implements ILocationEstimator {
      */
     @Override
     public PositionRecord getPosition(BeaconRecord beaconRecord) {
-        // Timestamp는 가장 최신의 정보로 설정
         long timestamp = beaconRecord.getScanners().stream()
                 .map(ScannerData::getTimestamp)
                 .max(Comparator.naturalOrder())
@@ -40,77 +54,38 @@ public class LocationTriangulator implements ILocationEstimator {
 
         return new PositionRecord(timestamp, beaconRecord.getPseudonym(), payloads, xy.getKey(), xy.getValue());
     }
-
     
-    public class KalmanFilter {
-        private boolean initialized;
-        private double processNoise;
-        private double measurementNoise;
-        private double predictedRSSI;
-        private double errorCovariance;
-    
-        public KalmanFilter(double processNoise, double measurementNoise) {
-            this.initialized = false;
-            this.processNoise = processNoise;
-            this.measurementNoise = measurementNoise;
-            this.predictedRSSI = 0;
-            this.errorCovariance = 0;
-        }
-
-        /**
-         * 칼만필터(KalmanFilter)를 통한 RSSI 전처리 
-         * @param rssi 필터링할 원본 RSSI 값
-         * @return 필터링된 RSSI 값
-         */
-        public double filter(double rssi) {
-            if (!initialized) {
-                initialized = true;
-                predictedRSSI = rssi;
-                errorCovariance = 1;
-            } else {
-                double priorErrorCovariance = errorCovariance + processNoise;
-                double kalmanGain = priorErrorCovariance / (priorErrorCovariance + measurementNoise);
-                predictedRSSI = predictedRSSI + (kalmanGain * (rssi - predictedRSSI));
-                errorCovariance = (1 - kalmanGain) * priorErrorCovariance;
-            }
-    
-            return predictedRSSI;
-        }
-    }
 
     /**
-     * 스캐너 데이터로부터 위치를 추정한다.
-     * @param scanners 스캐너로부터 수집된 데이터 리스트
-     * @return X와 Y 좌표를 포함하는 Map.Entry 객체, 여기서 Key는 X좌표, Value는 Y좌표
+     * BeaconRecord에서 제공된 스캐너 데이터를 사용하여 사용자의 위치를 추정한다.
+     * 이 메소드는 RSSI 값을 기반으로 가장 강한 신호를 보낸 상위 3개의 스캐너를 선택하고,
+     * 각 스캐너로부터의 거리를 계산하여 삼각측량을 수행한다.
+     * @param scanners BeaconRecord에 포함된 스캐너 데이터 목록
+     * @return 추정된 사용자의 위치 좌표 (x, y). Map.Entry 객체로, key는 x좌표, value는 y좌표를 나타낸다.
      */
     private Map.Entry<Long, Long> estimatePosition(List<ScannerData> scanners) {
-
-        KalmanFilter kalmanFilter = new KalmanFilter(0.005, 20); //new말고..바꿀 예정
-
-        Map<String, Point2D.Double> scannerPositions = new HashMap<>();
-        scannerPositions.put("A", new Point2D.Double(0, 0));
-        scannerPositions.put("B", new Point2D.Double(400, 0));  
-        scannerPositions.put("C", new Point2D.Double(0, 450));
-        scannerPositions.put("D", new Point2D.Double(400, 450));
-
         List<ScannerData> closestScanners = scanners.stream()
-        .sorted(Comparator.comparingInt(ScannerData::getRssi).reversed())
-        .limit(3)
-        .collect(Collectors.toList());
- 
+            .sorted(Comparator.comparingInt(ScannerData::getRssi).reversed())
+            .limit(3)
+            .collect(Collectors.toList());
+
         Point2D.Double[] points = new Point2D.Double[3];
         double[] distances = new double[3];
         for (int i = 0; i < 3; i++) {
             ScannerData scanner = closestScanners.get(i);
             points[i] = scannerPositions.get(scanner.getScannerId());
-            double filteredRSSI = kalmanFilter.filter(scanner.getRssi()); // 칼만 필터를 사용하여 RSSI 값을 필터링
-            distances[i] = calculateDistanceFromRssi(filteredRSSI);
+            distances[i] = calculateDistanceFromRssi(scanner.getRssi());
+
+            System.out.println("Scanner ID: " + scanner.getScannerId() + ", Distance: " + distances[i]);
         }
 
         Point2D.Double position = triangulate(points[0], distances[0], points[1], distances[1], points[2], distances[2]);
+        System.out.println("Estimated Position: x=" + position.x + ", y=" + position.y);
 
         return new AbstractMap.SimpleEntry<>((long) position.x, (long) position.y);
+
     }
+
 
     /**
      * RSSI 신호 강도를 거리로 변환한다.
@@ -118,51 +93,51 @@ public class LocationTriangulator implements ILocationEstimator {
      * @return 계산된 거리
      */
     private double calculateDistanceFromRssi(double rssi) {
-        double referenceRssi = -50.0; // 참조 RSSI 값
-        double pathLossParameter = 3.0; // 경로 손실 매개변수
-        double referenceDistance = 1.0; // 참조 거리
+        double referenceRssi = -50.0;
+        double pathLossParameter = 3.0;
+        double referenceDistance = 100.0;
     
         return Math.pow(10.0, (referenceRssi - rssi) / (10.0 * pathLossParameter)) * referenceDistance;
     }
 
+
     /**
      * 세 개의 스캐너 위치와 각 스캐너로부터의 거리를 기반으로 삼각측량을 수행한다.
-     * @param p1 첫 번째 스캐너의 위치
+     * 이 메소드는 세 점(스캐너 위치)과 각 점에서의 거리를 바탕으로 사용자의 위치를 계산한다.
+     * @param p1 첫 번째 스캐너의 위치 (Point2D.Double 객체)
      * @param d1 첫 번째 스캐너로부터의 거리
-     * @param p2 두 번째 스캐너의 위치
+     * @param p2 두 번째 스캐너의 위치 (Point2D.Double 객체)
      * @param d2 두 번째 스캐너로부터의 거리
-     * @param p3 세 번째 스캐너의 위치
+     * @param p3 세 번째 스캐너의 위치 (Point2D.Double 객체)
      * @param d3 세 번째 스캐너로부터의 거리
-     * @return 삼각측량을 통해 추정된 위치
+     * @return 삼각측량을 통해 추정된 사용자의 위치 (Point2D.Double 객체). 
+     *         만약 위치를 계산할 수 없는 경우 null을 반환할 수 있음.
      */
     private Point2D.Double triangulate(Point2D.Double p1, double d1, Point2D.Double p2, double d2, Point2D.Double p3, double d3) {
-        // 삼각측량 계산
-        // 중심 좌표와 각 비콘으로부터의 거리를 사용하여 비콘의 위치를 추정
-        double x, y;
-
-        double A = 2*p2.x - 2*p1.x;
-        double B = 2*p2.y - 2*p1.y;
+        // 선형 방정식 계산을 위한 변수
+        double A = 2 * (p2.x - p1.x);
+        double B = 2 * (p2.y - p1.y);
         double C = Math.pow(d1, 2) - Math.pow(d2, 2) - Math.pow(p1.x, 2) + Math.pow(p2.x, 2) - Math.pow(p1.y, 2) + Math.pow(p2.y, 2);
-        double D = 2*p3.x - 2*p2.x;
-        double E = 2*p3.y - 2*p2.y;
+        double D = 2 * (p3.x - p2.x);
+        double E = 2 * (p3.y - p2.y);
         double F = Math.pow(d2, 2) - Math.pow(d3, 2) - Math.pow(p2.x, 2) + Math.pow(p3.x, 2) - Math.pow(p2.y, 2) + Math.pow(p3.y, 2);
-
-        x = (C*E - F*B) / (E*A - B*D);
-        y = (C*D - A*F) / (B*D - A*E);
-
+    
+        // 사용자의 위치 (x, y) 계산
+        double x = ((F * B) - (E * C)) / ((B * D) - (E * A));
+        double y = ((F * A) - (D * C)) / ((A * E) - (D * B));
+    
         return new Point2D.Double(x, y);
     }
-
+    
 
     /**
      * Scanner들이 수신한 Payload를 단일 Payload로 정리한다.
      * 현재는 Battery와 Click여부만을 수집하므로 각 Key별 최댓값을 반환한다.
-     * @param scannerPayloads Scanner로부터 수신한 Payload 
-     * @return 단일 Payload 
+     * @param scannerPayloads Scanner로부터 수신한 Payload
+     * @return 단일 Payload
      */
     private Map<String, Integer> reducePayload(Map<Long, Map<String, Integer>> scannerPayloads) {
         Map<String, Integer> payloads = new HashMap<>();
-
         scannerPayloads.values()
                 .stream().flatMap(m -> m.entrySet().stream())
                 .forEach(e -> payloads.compute(e.getKey(),
--- src/test/java/bob/geunrobeol/platform/tech/PlatformTechApplicationTests.java
+++ src/test/java/bob/geunrobeol/platform/tech/PlatformTechApplicationTests.java
@@ -25,14 +25,54 @@ class PlatformTechApplicationTests {
 
 	@Test
 	void contextLoads() {
-		BeaconRecord record = new BeaconRecord();
-		List<ScannerData> data = new ArrayList<>();
-		data.add(new ScannerData(1, "A", -50));
-		data.add(new ScannerData(1, "B", -50));
-		data.add(new ScannerData(1, "C", -50));
-		data.add(new ScannerData(1, "D", -50));
-		record.setScanners(data);
-		PositionRecord posRec = locationTriangulator.getPosition(record);
-		log.info("PositionRecord: {}", posRec);
+		// 첫 번째 측위 대상자의 첫 번째 타임스탬프 데이터
+		BeaconRecord target1Record1 = new BeaconRecord();
+		List<ScannerData> target1Data1 = new ArrayList<>();
+		target1Data1.add(new ScannerData(1, "A", -65));
+		target1Data1.add(new ScannerData(1, "B", -80));
+		target1Data1.add(new ScannerData(1, "C", -25));
+		target1Data1.add(new ScannerData(1, "D", -40));
+		target1Record1.setScanners(target1Data1);
+
+		// 첫 번째 측위 대상자의 두 번째 타임스탬프 데이터
+		BeaconRecord target1Record2 = new BeaconRecord();
+		List<ScannerData> target1Data2 = new ArrayList<>();
+		target1Data2.add(new ScannerData(2, "A", -50));
+		target1Data2.add(new ScannerData(2, "B", -70));
+		target1Data2.add(new ScannerData(2, "C", -40));
+		target1Data2.add(new ScannerData(2, "D", -55));
+		target1Record2.setScanners(target1Data2);
+
+		// 두 번째 측위 대상자의 첫 번째 타임스탬프 데이터
+		BeaconRecord target2Record1 = new BeaconRecord();
+		List<ScannerData> target2Data1 = new ArrayList<>();
+		target2Data1.add(new ScannerData(1, "A", -25));
+		target2Data1.add(new ScannerData(1, "B", -65));
+		target2Data1.add(new ScannerData(1, "C", -40));
+		target2Data1.add(new ScannerData(1, "D", -80));
+		target2Record1.setScanners(target2Data1);
+
+		// 두 번째 측위 대상자의 두 번째 타임스탬프 데이터
+		BeaconRecord target2Record2 = new BeaconRecord();
+		List<ScannerData> target2Data2 = new ArrayList<>();
+		target2Data2.add(new ScannerData(2, "A", -40));
+		target2Data2.add(new ScannerData(2, "B", -25));
+		target2Data2.add(new ScannerData(2, "C", -80));
+		target2Data2.add(new ScannerData(2, "D", -65));
+		target2Record2.setScanners(target2Data2);
+
+		// 각 BeaconRecord에 대한 위치 추정
+		PositionRecord posRec1 = locationTriangulator.getPosition(target1Record1);
+		PositionRecord posRec2 = locationTriangulator.getPosition(target1Record2);
+		PositionRecord posRec3 = locationTriangulator.getPosition(target2Record1);
+		PositionRecord posRec4 = locationTriangulator.getPosition(target2Record2);
+
+		// 로그 출력
+		log.info("Target 1, Timestamp 1: {}", posRec1);
+		log.info("Target 1, Timestamp 2: {}", posRec2);
+		log.info("Target 2, Timestamp 1: {}", posRec3);
+		log.info("Target 2, Timestamp 2: {}", posRec4);
 	}
+
+
 }
